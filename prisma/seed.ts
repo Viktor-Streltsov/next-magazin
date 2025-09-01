253f65be0af45a978b580facf5d11fd8b90409e4
Viktor <viktor4strlsov@gmail.com>
2025-09-02 01:31:27 +0600

    Подготовув бека

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { hashSync } from 'bcrypt';
+import { prisma } from './prisma-client';
+
+async function up() {
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: 'User',
+        email: 'user@test.ru',
+        password: hashSync('111111', 10),
+        verified: new Date(),
+        role: 'USER',
+      },
+      {
+        fullName: 'Admin',
+        email: 'admin@test.ru',
+        password: hashSync('111111', 10),
+        verified: new Date(),
+        role: 'ADMIN',
+      },
+    ],
+  });
+}
+
+async function down() {}
+
+async function main() {
+  try {
+    await down();
+    await up();
+  } catch (e) {
+    console.error(e);
+  }
+}
