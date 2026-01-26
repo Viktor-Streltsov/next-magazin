cd90766bcf64f3420fa22668e6b74f23c46a2c05
Viktor <viktor4strlsov@gmail.com>
2026-01-26 23:24:24 +0600

    nextConfig

--- shared/hooks/use-query-filters.ts
+++ shared/hooks/use-query-filters.ts
@@ -1,23 +1,32 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Filters } from './use-filters';
 import qs from 'qs';
 import { useRouter } from 'next/navigation';
 
 export const useQueryFilters = (filters: Filters) => {
+  const isMounted = React.useRef(false);
   const router = useRouter();
 
-  useEffect(() => {
-    const params = {
-      ...filters.prices,
-      pizzaTypes: Array.from(filters.pizzaTypes),
-      sizes: Array.from(filters.sizes),
-      ingredients: Array.from(filters.selectedIngredients),
-    };
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const params = {
+        ...filters.prices,
+        pizzaTypes: Array.from(filters.pizzaTypes),
+        sizes: Array.from(filters.sizes),
+        ingredients: Array.from(filters.selectedIngredients),
+      };
 
-    const query = qs.stringify(params, {
-      arrayFormat: 'comma',
-    });
+      const query = qs.stringify(params, {
+        arrayFormat: 'comma',
+      });
 
-    router.push(`?${query}`, { scroll: false });
+      router.push(`?${query}`, {
+        scroll: false,
+      });
+
+      console.log(filters, 999);
+    }
+
+    isMounted.current = true;
   }, [filters]);
 };
